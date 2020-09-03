b2d0652f1ad0f960f5f75b89e70191be0356729b
Borris Lee <borrislee@live.com>
2020-09-03 18:49:12 -0400

    added styling to invalid textarea

--- src/character-counter.js
+++ src/character-counter.js
@@ -6,6 +6,8 @@ const CharacterCount = () => {
   textareaDom.addEventListener('input', () => {
 
     counterDom.textContent = 140 - textareaDom.value.length;
+
+    (counterDom.textContent === '0') ? counterDom.style.color = 'red' : counterDom.style.color = 'black';
   });
 };
 
